43312bf61b88acea335c87980d8ce8c9580194d7
Francesco Tenerini <tenero@gmail.com>
2025-04-23 10:21:42 +0200

    Clean code

--- csr_create.sh
+++ csr_create.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # https://github.com/GoogleCloudPlatform/kms-integrations/releases
-export PKCS11_MODULE_PATH="/home/francesco/src/test_py/kms/libkmsp11-1.6-linux-amd64/libkmsp11.so"
-export KMS_PKCS11_CONFIG="/home/francesco/src/test_py/kms/pkcs11-config.yaml"
+# FIXME: use absolute path
+export PKCS11_MODULE_PATH="./libkmsp11-1.6-linux-amd64/libkmsp11.so"
+export KMS_PKCS11_CONFIG="./pkcs11-config.yaml"
 
 
-export PROJECT_ID="siqtraq-208312"
+# FIXME: add project id
+export PROJECT_ID=""
 export LOCATION_ID="global"
 export KEY_RING_ID="test-hsm"
 export KEY_ID="tmp-hsm"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
+import sys
 
-from gc_kms import create_key_ring, create_key_hsm, sign_asymmetric, verify_asymmetric_rsa
+from gc_kms import create_key_ring, create_key_hsm, sign_asymmetric, \
+    verify_asymmetric_rsa
 
 PROJECT_ID = os.environ["PROJECT_ID"]
 LOCATION_ID = os.environ["LOCATION_ID"]
@@ -9,36 +11,45 @@ KEY_ID = os.environ["KEY_ID"]
 
 
 def main():
-    # create_key_ring(PROJECT_ID, LOCATION_ID, KEY_RING_ID)
-    # create_key_hsm(
-    #     project_id=PROJECT_ID,
-    #     location_id=LOCATION_ID,
-    #     key_ring_id=KEY_RING_ID,
-    #     key_id=KEY_ID,
-    # )
+    if sys.argv[1] == "create-key-ring":
+        create_key_ring(PROJECT_ID, LOCATION_ID, KEY_RING_ID)
+        create_key_hsm(
+            project_id=PROJECT_ID,
+            location_id=LOCATION_ID,
+            key_ring_id=KEY_RING_ID,
+            key_id=KEY_ID,
+        )
+        return
 
     with open("bar.txt", "r") as f:
         text = f.read()
-    # sign_asymmetric(
-    #     project_id=PROJECT_ID,
-    #     location_id=LOCATION_ID,
-    #     key_ring_id=KEY_RING_ID,
-    #     key_id=KEY_ID,
-    #     version_id="1",
-    #     message=text
-    # )
 
-    with open("bar.txt.sign", "rb") as f:
-        sign = f.read()
-    verify_asymmetric_rsa(
-        project_id=PROJECT_ID,
-        location_id=LOCATION_ID,
-        key_ring_id=KEY_RING_ID,
-        key_id=KEY_ID,
-        version_id="1",
-        signature=sign,
-        message=text,
-    )
+    if sys.argv[1] == "sign-asymmetric":
+        sign_asymmetric(
+            project_id=PROJECT_ID,
+            location_id=LOCATION_ID,
+            key_ring_id=KEY_RING_ID,
+            key_id=KEY_ID,
+            version_id="1",
+            message=text
+        )
+    elif sys.argv[1] == "verify-asymmetric-rsa":
+        with open("bar.txt.sign", "rb") as f:
+            sign = f.read()
+        verify_asymmetric_rsa(
+            project_id=PROJECT_ID,
+            location_id=LOCATION_ID,
+            key_ring_id=KEY_RING_ID,
+            key_id=KEY_ID,
+            version_id="1",
+            signature=sign,
+            message=text,
+        )
+    else:
+        print(
+            "Unknown command, use create-key-ring or sign-asymmetric "
+            "or verify-asymmetric-rsa"
+        )
 
 
 if __name__ == "__main__":
